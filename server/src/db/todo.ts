a0a7fd1ff7653e16d3468f74c872f9e6073880a2
LearnShare <learnshare@126.com>
2023-12-25 12:53:29 +0800

    feat: support update and delete todo.

--- server/src/db/todo.ts
+++ server/src/db/todo.ts
@@ -7,12 +7,12 @@ const db = prisma.todo;
 
 // get todos
 function list({
-  size,
-  page,
+  // size,
+  // page,
 }) {
   return db.findMany({
-    skip: size * (page - 1),
-    take: size,
+    // skip: size * (page - 1),
+    // take: size,
   });
 }
 
@@ -38,8 +38,36 @@ function create({
   });
 }
 
+// udpate todo
+function update({
+  id,
+  text,
+}) {
+  return db.update({
+    data: {
+      text,
+    },
+    where: {
+      id,
+    },
+  });
+}
+
+// delete todo
+function del({
+  id,
+}) {
+  return db.delete({
+    where: {
+      id,
+    },
+  });
+}
+
 export default {
   list,
   get,
   create,
+  update,
+  del,
 };
--- server/src/router/todo/index.ts
+++ server/src/router/todo/index.ts
@@ -9,10 +9,10 @@ const todoRouter = Router();
 // get todos
 todoRouter.get('/', async (req: Request, res: Response) => {
   const list = await DB.todo.list({
-    size: Number(req.query.size)
-      || 10,
-    page: Number(req.query.page)
-      || 1,
+    // size: Number(req.query.size)
+    //   || 10,
+    // page: Number(req.query.page)
+    //   || 1,
   });
 
   res.json(list);
@@ -49,12 +49,30 @@ todoRouter.post('/', async (req: Request, res: Response) => {
 });
 
 // update todo
-todoRouter.put(`/:id`, (req: Request, res: Response) => {
-  res.json(req.body);
+todoRouter.put(`/:id`, async (req: Request, res: Response) => {
+  const {
+    id,
+  } = req.params;
+  const {
+    text,
+  } = req.body;
+  const todo = await DB.todo.update({
+    id: Number(id),
+    text,
+  });
+
+  res.json(todo);
 });
 
 // delete todo
-todoRouter.delete(`/:id`, (req: Request, res: Response) => {
+todoRouter.delete(`/:id`, async (req: Request, res: Response) => {
+  const {
+    id,
+  } = req.params;
+  await DB.todo.del({
+    id: Number(id),
+  });
+
   res.end();
 });
 
